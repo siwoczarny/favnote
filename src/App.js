624243bbb9344eb70e79fa03733035ec32a18b3e
Maciej Chrustowski <chrustowski.maciej@gmail.com>
2020-04-02 17:20:59 +0200

    Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
 import './App.css';
+
+const MyButton = styled.button`
+  border: 2px solid #61dbfb;
+  padding: 48px;
+  border-radius: 8px;
+`;
 
 const App = () => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
+      <MyButton>Click this!</MyButton>
       <a
         className="App-link"
         href="https://reactjs.org"
