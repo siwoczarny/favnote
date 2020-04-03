f7e7d8d7009ec84a811a57c42b7f52f2b88c6346
Maciej Chrustowski <chrustowski.maciej@gmail.com>
2020-04-04 00:33:37 +0200

    Storybook knobs

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { withKnobs, select } from '@storybook/addon-knobs/react';
 import Button from './Button';
 
 export default {
   title: 'Button',
   component: Button,
+  decorators: [withKnobs],
 };
 
-export const Primary = () => <Button>Hello Siwoczarny</Button>;
+export const Primary = () => {
+  const label = 'Colors';
+  const options = {
+    Primary: 'hsl(49, 100%, 58%)',
+    Secondary: 'hsl(196, 83%, 75%)',
+    Tertiary: 'hsl(106, 47%, 64%)',
+  };
+  const defaultValue = 'hsl(49, 100%, 58%)';
+  const groupId = 'GROUP-ID1';
+  const value = select(label, options, defaultValue, groupId);
+  return <Button color={value}>Hello Siwoczarny</Button>;
+};
 export const Secondary = () => <Button secondary>Hello Siwoczarny</Button>;
