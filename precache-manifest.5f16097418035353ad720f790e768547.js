b8afae19a1acc7c32f026d8fcfcd17f9738ffd86
Maciej Chrustowski <chrustowski.maciej@gmail.com>
2020-04-15 20:11:54 +0200

    Updates

---
+++ precache-manifest.5f16097418035353ad720f790e768547.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ade9348f4c9223d69f6a3401f1be3e39",
+    "url": "/favnote/index.html"
+  },
+  {
+    "revision": "b04c77d184b18aaeef05",
+    "url": "/favnote/static/js/2.68d5d362.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/favnote/static/js/2.68d5d362.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43333d255ab4c9d80ec2",
+    "url": "/favnote/static/js/main.78fc0fde.chunk.js"
+  },
+  {
+    "revision": "6bfcfd2e3aa0ccca8a2d",
+    "url": "/favnote/static/js/runtime-main.5ecfa962.js"
+  },
+  {
+    "revision": "af737b5505b4cdb4a433cc3ac1f234af",
+    "url": "/favnote/static/media/bulb.af737b55.svg"
+  },
+  {
+    "revision": "022222e6389603a7a5cb48de60061c1a",
+    "url": "/favnote/static/media/link.022222e6.svg"
+  },
+  {
+    "revision": "93d4f3ba834e7afdf3f8dcb93f2a82f8",
+    "url": "/favnote/static/media/logo.93d4f3ba.svg"
+  },
+  {
+    "revision": "95745c59f00a0c06399781cb5f0852fe",
+    "url": "/favnote/static/media/logout.95745c59.svg"
+  },
+  {
+    "revision": "6fbd708eecd64af11dc9864b26be6377",
+    "url": "/favnote/static/media/magnifier.6fbd708e.svg"
+  },
+  {
+    "revision": "554c0ca363eff0ab2ea2e5ad614e0c67",
+    "url": "/favnote/static/media/pen.554c0ca3.svg"
+  },
+  {
+    "revision": "4f002e82601e5d675a16c9e9074e0e15",
+    "url": "/favnote/static/media/plus.4f002e82.svg"
+  },
+  {
+    "revision": "051af8ba4d1984fc498ff6da805311cb",
+    "url": "/favnote/static/media/twitter.051af8ba.svg"
+  }
+]);
